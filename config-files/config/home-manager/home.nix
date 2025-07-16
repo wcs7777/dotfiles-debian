d3c6b3f82bf5df9102a1854bea124316f9d3dd5a
wcs7777 <wcs777git@gmail.com>
2025-07-16 01:07:56 -0300

    add more apt and nix packages

--- config-files/config/home-manager/home.nix
+++ config-files/config/home-manager/home.nix
@@ -5,12 +5,19 @@
     programs.home-manager.enable = true;
     home.packages = [
         pkgs.bat
+        pkgs.cargo
         pkgs.delta
         pkgs.eza
         pkgs.fd
         pkgs.fzf
+        pkgs.go
+        pkgs.lua
+        pkgs.luajitPackages.luarocks_bootstrap
         pkgs.nodejs
         pkgs.ripgrep
+        pkgs.ruby
+        pkgs.sqlite
         pkgs.trash-cli
+        pkgs.tree-sitter
     ];
 }
