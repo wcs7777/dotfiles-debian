9e54b6e9293cc1f2d83a61127bc3160b76907c81
wcs7777 <wcs777git@gmail.com>
2025-07-25 01:37:57 -0300

    update home.nix and harpoon_paths.zsh

--- config-files/config/home-manager/home.nix
+++ config-files/config/home-manager/home.nix
@@ -1,19 +1,23 @@
-{pkgs, ...}: {
-    home.username = "wcs";
-    home.homeDirectory = "/home/wcs";
-    home.stateVersion = "24.11"; # Comment out for error with "latest" version
-    programs.home-manager.enable = true;
-    home.packages = [
-        pkgs.bat
-        pkgs.cargo
-        pkgs.delta
-        pkgs.eza
-        pkgs.fd
-        pkgs.fzf
-        pkgs.ripgrep
-        pkgs.ruby
-        pkgs.sqlite
-        pkgs.trash-cli
-        pkgs.tree-sitter
-    ];
+{
+  home.username = "wcs";
+  home.homeDirectory = "/home/wcs";
+  home.stateVersion = "25.05"; # Please read the comment before changing.
+  home.packages = [
+    pkgs.bat
+    pkgs.cargo
+    pkgs.delta
+    pkgs.eza
+    pkgs.fd
+    pkgs.fzf
+    pkgs.ripgrep
+    pkgs.ruby
+    pkgs.sqlite
+    pkgs.trash-cli
+    pkgs.tree-sitter
+  ];
+  home.file = {
+  };
+  home.sessionVariables = {
+  };
+  programs.home-manager.enable = true;
 }
