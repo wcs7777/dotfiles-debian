4df8e9d2c96b1a5d8b62adfd68961bef182796af
wcs7777 <wcs777git@gmail.com>
2025-12-06 02:06:05 -0300

    fix(install-mssql-odbc-cli): use Debian version 12 while 13 is not supported

--- setup-scripts/install-mssql-odbc-cli.sh
+++ setup-scripts/install-mssql-odbc-cli.sh
@@ -5,7 +5,8 @@ if command -v sqlcmd &> /dev/null; then
 	exit 0
 fi
 
-DEBIAN_VERSION=$(lsb_release -rs)
+# DEBIAN_VERSION=$(lsb_release -rs)
+DEBIAN_VERSION=12
 curl -sLSO https://packages.microsoft.com/config/debian/$DEBIAN_VERSION/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
