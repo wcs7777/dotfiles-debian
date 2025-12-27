c57b952611faecbf0a9132383fc7020c913630a0
wcs7777 <wcs777git@gmail.com>
2025-12-27 20:54:28 -0300

    update harpoon_paths

--- config-files/my/zsh/plugins/harpoon_paths.zsh
+++ config-files/my/zsh/plugins/harpoon_paths.zsh
@@ -1,11 +1,15 @@
 __my_paths_array=(
-	~/.config/nvim/ # 1
-	~/.dotfiles     # 2
-	~               # 3
-	~               # 4
-	~               # 5
-	~               # 6
-	~               # 7
-	~               # 8
-	~               # 9
+	~/data/projects/pronunciation/dev/ # 1
+	~/data/projects/pronunciation/chrome/ # 1
+	~/data/projects/pronunciation/build/ # 1
+	~/data/projects/pronunciation/main/ # 1
+	~/.config/nvim/                    # 2
+	~/.dotfiles                        # 3
+	~                                  # 4
+	~                                  # 5
+	~                                  # 6
+	~                                  # 7
+	~                                  # 8
+	~                                  # 9
+	~                                  # 10
 )
