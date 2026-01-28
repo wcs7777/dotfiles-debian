05ed15e7585632ecbdf36b13ee207d1f51366d84
wcs7777 <wcs777git@gmail.com>
2026-01-28 00:56:14 -0300

    zshrc: support private harpoon_paths

--- config-files/my/zsh/plugins/harpoon_paths.zsh
+++ config-files/my/zsh/plugins/harpoon_paths.zsh
@@ -1,15 +1,11 @@
 __my_paths_array=(
-	~/data/projects/pronunciation/dev/ # 1
-	~/data/projects/pronunciation/chrome/ # 1
-	~/data/projects/pronunciation/build/ # 1
-	~/data/projects/pronunciation/main/ # 1
-	~/.config/nvim/                    # 2
-	~/.dotfiles                        # 3
-	~                                  # 4
-	~                                  # 5
-	~                                  # 6
-	~                                  # 7
-	~                                  # 8
-	~                                  # 9
-	~                                  # 10
+	~ #1
+	~ #2
+	~ #3
+	~ #4
+	~ #5
+	~ #6
+	~ #7
+	~ #8
+	~ #9
 )
