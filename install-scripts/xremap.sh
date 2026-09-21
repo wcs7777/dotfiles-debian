277253c12a2c1d5b13c2046daf6290632cde2a98
wcs7777 <wcs777git@gmail.com>
2026-09-20 21:07:42 -0300

    update xremap install script

--- install-scripts/xremap.sh
+++ install-scripts/xremap.sh
@@ -22,7 +22,3 @@ unzip "$file" -d "$dir"
 sudo install "$dir/xremap" -Dm755 /usr/local/bin/xremap
 
 # https://extensions.gnome.org/extension/5060/xremap/
-
-sudo modprobe uinput
-echo uinput | sudo tee /etc/modules-load.d/uinput.conf
-echo 'ACTION=="add", SUBSYSTEM=="input", ATTRS{name}=="xremap", ENV{ID_INPUT_MOUSE}="1"' | sudo tee /etc/udev/rules.d/99-xremap-mouse.rules
